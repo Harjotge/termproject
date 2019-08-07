07914562f8564f797dc8d99d4098f41df6e775fa
Harjot Singh Na <747998@student.tor.loc>
2019-08-07 17:03:20 -0400

    added bodyparser

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.get('/',function(req,res){
 res.send('<h1>Hello World</h1>')
 })
